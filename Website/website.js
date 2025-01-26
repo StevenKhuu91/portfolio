6e37f96600678d81c79ab2a2982075f557014172
Steven Khuu <48769141+StevenKhuu91@users.noreply.github.com>
2025-01-25 21:33:39 -0600

    bento grid foundation (no need to include JS for now just work on it as is.)

--- Website/website.js
+++
@@ -1,8 +0,0 @@
-var name = prompt("What is your name?");
-var firstChar = name.slice(0,1);
-var upperCaseFirstChar = firstChar.toUpperCase();
-var restOfName = name.slice(1,name.length);
-var lowerCaseName = restOfName.toLowerCase();
-var fixedName = upperCaseFirstChar + lowerCaseName;
-alert("Hello " + fixedName);
-
