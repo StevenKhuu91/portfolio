c8330aed7cc14b99463a8eaf4fe272fcd484f218
Steven Khuu <48769141+StevenKhuu91@users.noreply.github.com>
2025-02-03 12:34:41 -0600

    website redesign for mobile first approach w/ light/dark theme JS added

--- Website/script.js
+++ Website/script.js
@@ -1,12 +1,12 @@
-/*-- TOOLTIPS --*/
-
-var tooltip = document.querySelectorAll('.tooltip');
-
-document.addEventListener('mousemove', ttp, false);
-
-function ttp(e) {
-    for (var i=tooltip.length; i--;) {
-    tooltip[i].style.left = e.pageX + 100 + 'px';
-    tooltip[i].style.top = e.pageY + -20 + 'px';
+/*--- LIGHT/DARK MODE ---*/
+function setTheme(theme) {
+    let root = document.documentElement;
+    if (theme === 'light') {
+        root.style.setProperty('--bg-color', '#fff');
+    }
+    else if (theme === 'dark') {
+        root.style.setProperty('--bg-color', '#000');
     }
 }
+/* See how this uses root styles! Very similar to CSS roots, and uses them in fact. */
+setTheme('light');
